eaef3b8f19d11cc89827a252236199148491ae10
imamkabir <imamkabir397@gmail.com>
2025-12-29 15:07:34 -0500

    Update: Added new files and practice code

--- array.php
+++ array.php
@@ -14,3 +14,11 @@ array_push($fruits, 'bana32');
 // add to the beagrning 
 array_unshift($fruits, 'banaereeeeeeeeeee');
 // remve from array 
+array_pop($fruits);
+// basicly it takes off the last part of it so 
+array_shift($fruits);
+// take off  from th beagrning ok 
+// remove a specofic element 
+unset($fruits[2]);
+// split into 2 chuncks 
+$chuncked_array =  array_chunk($fruits, 2);
