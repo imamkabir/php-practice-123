e593cd6860da43bcb7beb213812b4b943d2b2a37
imamkabir <imamkabir397@gmail.com>
2026-01-07 16:13:16 +0100

    b

--- new/date.php
+++ new/date.php
@@ -1,3 +1,5 @@
 <?php
+// Set timezone to Lagos and display current date and time
 echo date('Y-m-d H:i:s');
-a
+// Set the default timezone to Africa/Lagos
+date_default_timezone_set('Africa/Lagos');
