2c8af2f1d6425971aea3201509b72c76e8a44e07
imamkabir <imamkabir397@gmail.com>
2026-01-16 15:59:08 +0100

    t

--- new/game.php
+++ new/game.php
@@ -12,7 +12,7 @@ class Game {
     public function getGameName() {
         return $this->gameName;
     }
-// --- IGNORE ---   ignore this line end ---u       u   k   l   nnb g   d   eruohuohfffnsonfjsnfjnfonfronfrofroiwfjoifjofsonjdsnvdsjvjfsbsfhbjsdvjsdvjhusdfeuhsuhfeuwhherbgqe huwerhurehturhfuehtuerheugeurh;eruighrebgjrbvjrngjfngjrehgrujgnrejghreiugregrhgbherjdnvjsdnjohfieurhteiurhiuerhgeuirhtiuervneiurytueihtuierhtuierhtpw39iherotuhreuiteriuhreuihureihturepwireuhwuierwghreuihviuerhgreiuhjeroivjeroijdfvdskhvberiguiewrsddeaj;ajkbvnvj;uoietgtohfnvcn.kvjkrekjerbgiuhgwuhsdjnjdnfjwrenfsjnfwoejwiojfewiojfwieofhorhjiouergreyetiudf q3nvn0[vbxmf3j40rvhfhuhwqoiqwp[ieroptipoeiykfdjgla's;kfnxcm nvekfbwiu;ehgoiepurhfiwoehjfiweojhoi;jf]]
+
     public function getGameType() {
         return $this->gameType;
     }
